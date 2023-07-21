cb96783e605b66f8bb321853a40dc0560d3e71cb
vaishnaviamrutkar08 <vaishnaviamrutkar02@gmail.com>
2023-07-22 00:32:35 +0530

    first commit

--- src/org/tnsif/intro/UserInput.java
+++ src/org/tnsif/intro/UserInput.java
@@ -1,11 +1,31 @@
 package org.tnsif.intro;
 
+import java.util.Scanner;
+
 public class UserInput {
 
 	public static void main(String[] args) {
-		System.out.println("Amrutkar");
-		System.out.println("Vaishu");
+Scanner s = new Scanner(System.in);
 		
+		//number
+		System.out.println("Enter the number;");
+		int num = s.nextInt();
+		System.out.println("here is the given value: "+num);
+		
+		//float number
+		System.out.println("Enter the value of floatnum");
+		float num1=s.nextFloat();
+		System.out.println("The value of floatnum is: "+num1);
+		
+		//character
+		System.out.println("Enter the Character");
+		char c=s.next().charAt(1);
+		System.out.println("Character is: "+c);
+		
+		//string
+		System.out.println("Enter the String");
+		String str1=s.next();
+		System.out.println("String is:" +str1);
 	}
 
 }
