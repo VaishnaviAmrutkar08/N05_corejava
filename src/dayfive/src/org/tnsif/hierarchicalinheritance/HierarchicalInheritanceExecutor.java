f9a435e14ef07ee845719d35e97ba8c5442d4395
VaishnaviAmrutkar08 <126854209+VaishnaviAmrutkar08@users.noreply.github.com>
2023-08-06 23:19:02 +0530

    fifth commit

---
+++ src/dayfive/src/org/tnsif/hierarchicalinheritance/HierarchicalInheritanceExecutor.java
@@ -0,0 +1,19 @@
+package org.tnsif.hierarchicalinheritance;
+
+public class HierarchicalInheritanceExecutor {
+	
+public static void main(String[] args) {
+		
+		Tiramisu t=new Tiramisu("OnePlus","DoubleSlot",13);
+		SnowCone s=new SnowCone("Samsung","DoubleSlot",12);
+		
+		System.out.println(t);
+		System.out.println(s);
+		 
+
+				
+		
+
+	}
+
+}
---
+++ src/dayfive/src/org/tnsif/multilevelinheritance/MultilevelInheritanceExecutor.java
@@ -0,0 +1,14 @@
+package org.tnsif.multilevelinheritance;
+
+public class MultilevelInheritanceExecutor {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TeamMember m=new TeamMember("IT","Sans",2213,"Riya",1233,
+				"AI-autoDetection",5,15);
+		System.out.println(m);
+
+	}
+
+
+}
